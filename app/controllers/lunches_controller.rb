8ba020661cdfcb96bc31e521f32e6f1a88a86d62
Lamia Belkacem <lamia.belkacem86@gmail.com>
2022-12-06 13:46:30 -0500

    actions controller lunches

--- app/controllers/lunches_controller.rb
+++ app/controllers/lunches_controller.rb
@@ -1,10 +1,11 @@
 class LunchesController < ApplicationController
+  before_action :set_lunch, only: %i[show edit update destroy]
+
   def index
     @lunches = policy_scope(Lunch)
   end
 
   def show
-    authorize @lunch
   end
 
   def new
@@ -14,19 +15,49 @@ class LunchesController < ApplicationController
 
   def create
     @lunch = Lunch.new(lunch_params)
-    @luch.user = current_user
+    @lunch.user = current_user
     authorize @lunch
+    if @lunch.save
+      redirect_to root_path, notice: 'Your lunch was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-    authorize @lunch
   end
 
   def update
-    authorize @lunch
+    if @lunch.update(edit_lunch_params)
+      if params[:lunch][:photos].present?
+        params[:lunch][:photos].each do |photo|
+          @lunch.photos.attach(photo)
+        end
+      end
+      redirect_to @lunch, notice: "Your lunch was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @lunch.destroy
+    redirect_to group_lunches_path, status: :see_other
+  end
+
+  private
+
+  def set_lunch
+    @lunch = Lunch.find(params[:id])
     authorize @lunch
   end
+
+  def edit_lunch_params
+    params.require(:lunch).permit(:cooking_date, :title, :description, :recipe_url, :tags)
+  end
+
+  def lunch_params
+    params.require(:lunch).permit(:cooking_date, :title, :description, :recipe_url, :tags, photos: [])
+  end
+
 end

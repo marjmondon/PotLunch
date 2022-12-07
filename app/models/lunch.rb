415841960ea18504698aec81ea833e9e5aa3f64d
Florian Guillou <guilflo@hotmail.fr>
2022-12-07 13:00:56 -0600

    Beautiful seeds

--- app/models/lunch.rb
+++ app/models/lunch.rb
@@ -2,7 +2,7 @@ class Lunch < ApplicationRecord
   has_many_attached :photos
   belongs_to :group
   belongs_to :user
-
+  has_many :swaps
 
   validates :cooking_date, presence: true
   validates :title, presence: true

0f446c5dc6c7da3fd0cd66ec3d3f855853c9b692
Florian Guillou <guilflo@hotmail.fr>
2022-12-07 12:08:18 -0600

    Dashboard back-end
    On dashboard, displayed mylunches/Myswaps/Myswaps-asked and possibility to accepted/declined swap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
 
+  def all_users
+    User.where.not id: current_user.id
+  end
+
   # Pundit: allow-list approach
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?

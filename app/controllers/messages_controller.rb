2a16509314ddba24673e33857e6b1fc86bf648ac
Marjorie MONDON <marjmondon@hotmail.com>
2022-12-07 13:52:25 -0500

    finalisation du chat manque juste le css

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,16 @@ class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
+    authorize @message
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      redirect_to group_lunch_swap_chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: {message: @message})
+      )
+      head :ok
+      # redirect_to group_lunch_swap_chatroom_messages_path(@chatroom)
     else
       render "chatrooms/show", status: :unprocessable_entity
     end

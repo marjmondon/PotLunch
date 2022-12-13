84b12ba8bef63021b65adeec3368a756d7c99c31
Lamia Belkacem <lamia.belkacem86@gmail.com>
2022-12-13 11:42:12 -0500

    style My Lunches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_154141) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_192609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -77,6 +77,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_154141) do
     t.bigint "swap_id"
     t.index ["swap_id"], name: "index_messages_on_swap_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
   create_table "swaps", force: :cascade do |t|

3ed68396d62fcc49d3e07b1a7442a05d717674a5
Florian Guillou <guilflo@hotmail.fr>
2022-12-15 09:23:21 -0600

    in process

---
+++ app/javascript/controllers/active_current_group_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="active-current-group"
+export default class extends Controller {
+  static targets = ["group"]
+
+  connect() {
+    // console.log("Hello");
+    // console.log(this.groupTargets);
+  }
+
+  inlight(event) {
+    event.preventDefault()
+    // console.log(event);
+
+    this.groupTargets.forEach((groupTarget) => {
+      groupTarget.classList.remove("border");
+    });
+    event.currentTarget.classList.add('border');
+  }
+
+}

1d2b6f8cbb43cba2edf5d315a5a885ed50a9b8f0
Florian Guillou <guilflo@hotmail.fr>
2022-12-15 10:06:14 -0600

    pas de stimulus group too late
    bugs pour les coins fixed a verifier

--- app/javascript/controllers/active_current_group_controller.js
+++ app/javascript/controllers/active_current_group_controller.js
@@ -2,21 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="active-current-group"
 export default class extends Controller {
-  static targets = ["group"]
+  // static targets = ["group"]
 
   connect() {
     // console.log("Hello");
     // console.log(this.groupTargets);
   }
 
-  inlight(event) {
-    // event.preventDefault()
-    // console.log(event);
+  // inlight(event) {
+  //   // event.preventDefault()
+  //   // console.log(event);
 
-    this.groupTargets.forEach((groupTarget) => {
-      groupTarget.classList.remove("border");
-    });
-    event.currentTarget.classList.add('border');
-  }
+  //   this.groupTargets.forEach((groupTarget) => {
+  //     groupTarget.classList.remove("border");
+  //   });
+  //   event.currentTarget.classList.add('border');
+  // }
 
 }
